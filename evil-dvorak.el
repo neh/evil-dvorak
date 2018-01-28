505ec49e384b79031b5125255b05eb8b8ed24ec6
Nathan Howell <nath@nhowell.net>
2018-01-27 19:15:35 -0800

    Use regular vim-style motions
    htns = <left><down><up><right>

--- evil-dvorak.el
+++ evil-dvorak.el
@@ -58,8 +58,8 @@
 
 (evil-define-key 'visual evil-dvorak-mode-map
   "t" 'evil-next-line
-  "h" 'evil-previous-line
-  "n" 'evil-backward-char
+  "n" 'evil-previous-line
+  "h" 'evil-backward-char
   "s" 'evil-forward-char
   ;;I what to be able to use vaw (visual around word) and viw (visual inner word)
   ;; that's why in visual mode, u and a are not defined.
@@ -73,8 +73,8 @@
 (evil-define-key 'normal evil-dvorak-mode-map
   ;; Miscellancus
   (kbd "t") #'evil-next-line
-  (kbd "h") #'previous-line
-  (kbd "n") #'backward-char
+  (kbd "n") #'previous-line
+  (kbd "h") #'backward-char
   (kbd "s") #'forward-char
   "k" 'kill-line
   "K" #'(lambda () (interactive)
@@ -86,11 +86,6 @@
 
   ;;line manipulation
   "J" 'join-line
-  "j" #'(lambda () (interactive)
-          "join this line at the end of the line below"
-          (join-line 1))
-  (kbd "C-h") 'evil-open-below
-  (kbd "C-t") 'evil-open-above
   "'" 'evil-goto-mark
   ;;there is no need to set return to newline-and-indent, because electric-indent-mode is now on by default.
   (kbd "<return>") 'newline-and-indent)
@@ -101,9 +96,9 @@
   (kbd "C-d") 'delete-char
   (kbd "<backspace>") 'delete-backward-char
   (kbd "<return>") 'newline-and-indent
-  (kbd "C-h") 'evil-next-line
-  (kbd "C-t") 'evil-previous-line
-  (kbd "C-n") 'backward-char
+  (kbd "C-t") 'evil-next-line
+  (kbd "C-n") 'evil-previous-line
+  (kbd "C-h") 'backward-char
   (kbd "C-s") 'forward-char)
 
 (provide 'evil-dvorak)
